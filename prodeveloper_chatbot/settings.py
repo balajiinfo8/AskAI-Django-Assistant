d8742614349a1ce1f85e4f035e438887e473ee71
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-11-30 22:35:01 +0530

    Developed

--- prodeveloper_chatbot/settings.py
+++ prodeveloper_chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n$hjk$zw!^%o+l8fh_ne&$$qv5kby+_d92o5e1xqn+i%xvl1i2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [' .vercel.app','*']
+ALLOWED_HOSTS = ['askai-django-assistant.onrender.com','*']
 
 
 # Application definition
